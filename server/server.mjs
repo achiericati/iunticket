01e3d81d501d465bf5e4ee571a10d14cc71e05e2
Andrea  Chiericati <andreachiericati@Air-di-Andrea.station>
2024-03-04 14:48:44 +0100

    fix

--- server/server.mjs
+++ server/server.mjs
@@ -9,7 +9,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-app.use(forceSsl);
 app.use(express.static(join(__dirname, '../build')));
 
 app.use(function(req, res, next) {
