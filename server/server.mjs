d02d855e0ad897111fc16686f6bc433653def280
Andrea  Chiericati <andreachiericati@Air-di-Andrea.station>
2024-03-04 15:03:06 +0100

    fix

--- server/server.mjs
+++ server/server.mjs
@@ -11,12 +11,12 @@ const __dirname = dirname(__filename);
 const app = express();
 app.use(express.static(join(__dirname, '../build')));
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   if(!req.secure) {
       return res.redirect(['https://', req.get('Host'), req.url].join(''));
   }
   next();
-});
+});*/
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, '../build', 'index.html'));
